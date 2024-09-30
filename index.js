24d41cfca9942d84aad860da12fa9a85216340bd
Benoît de Biolley <benoit@lwt.be>
2024-09-30 18:07:44 +0200

    :sparkles: Update one record

--- index.js
+++ index.js
@@ -18,13 +18,22 @@ const databaseConfigs = {
   console.log("Connected!");
   await connection.beginTransaction();
   try {
-    // const sql = "UPDATE customers SET address = 'Canyon 123' WHERE address = 'Valley 345'";
-    // const result = await connection.query(sql);
-    // console.log(result.affectedRows + " record(s) updated");
+    const aantalKm = 10;
+    const rijksregisternummer = "";
+    const sql = "UPDATE aanwezigheden SET z29 = 1, Kms = Kms + ?, punten = punten + 10, bedrag2022 = bedrag2022 + 0.5, totaalbedrag = totaalbedrag + 0.5, totaalpunten = totaalpunten + 10, aanweztotaal = aanweztotaal + 1 WHERE rijksregisternummer = ?;";
 
+    const result = await connection.query(
+      sql,
+      [aantalKm, rijksregisternummer],
+    );
+    console.log(result[0].affectedRows + " record(s) updated");
+    if (result[0].affectedRows !== 1) {
+      console.error(`User ${rijksregisternummer} not found in our database`)
+    }
     // await connection.commit()
     await connection.rollback();
   } catch (e) {
+    console.error("Unexpected error ocurred", e);
     await connection.rollback();
   }
 
